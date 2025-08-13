47c28bb712f375b1d59a6339e7f2bb188b74354e
nayera sami <nayerasamy19@gmail.com>
2025-08-13 18:02:02 +0300

    intro page

--- src/app/components/sleek-intro/sleek-intro.component.ts
+++ src/app/components/sleek-intro/sleek-intro.component.ts
@@ -1,13 +1,14 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
-import { SliderComponent } from '@nayerasami/sleek-slider';
 
 @Component({
   selector: 'app-sleek-intro',
-  imports: [SliderComponent],
+  imports: [],
   templateUrl: './sleek-intro.component.html',
   styleUrl: './sleek-intro.component.scss',
 })
 export class SleekIntroComponent {
+  constructor(public router:Router){}
   responsiveOptions: any[] = [
     { breakpoint: '1400px', numVisible: 3, numScroll: 2 },
     { breakpoint: '1199px', numVisible: 3, numScroll: 1 },
@@ -34,7 +35,7 @@ export class SleekIntroComponent {
   imports: [SliderComponent],
   })
   items = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  
+
   options = {
     numberOfVisibleItems: 4,
     rows: 2,
@@ -43,7 +44,7 @@ export class SleekIntroComponent {
     animationSpeed: '0.6s',
     animation: 'linear'
   };
-  
+
   responsiveOptions = [
     { breakpoint: '1024px', numVisible: 2, numScroll: 1 },
     { breakpoint: '768px', numVisible: 1, numScroll: 1 }
