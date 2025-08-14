45e2e0c4467949828171fa8d41140283c3d664c0
nayera sami <nayerasamy19@gmail.com>
2025-08-14 16:38:37 +0300

    `feat: updated documentation and theme toggle functionality`

--- src/app/components/hearder/hearder.component.ts
+++ src/app/components/hearder/hearder.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import {  Router, RouterModule } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, Renderer2 } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-hearder',
@@ -8,5 +9,22 @@ import {  Router, RouterModule } from '@angular/router';
   styleUrl: './hearder.component.css'
 })
 export class HearderComponent {
-  constructor(public router : Router){}
+  constructor(public router: Router,@Inject(DOCUMENT) private document: Document,private renderer: Renderer2) { }
+  isLightTheme = false;
+
+  ngAfterViewInit() {
+    console.log(this.document.body.classList, 'classlist in ngAfterViewInit');
+  }
+  toggleTheme() {
+    console.log(this.document.body.classList, 'classlist');
+    this.isLightTheme = !this.isLightTheme;
+
+    if (this.isLightTheme) {
+      this.renderer.addClass(this.document.body, 'light-theme');
+    } else {
+      this.renderer.removeClass(this.document.body, 'light-theme');
+    }
+  }
+
+
 }
